a57428b1667f68812b2188cbe0840eb8ff7aaae7
joseph Omer <jodeveloper8@gmail.com>
2022-08-31 15:07:51 +0300

    set up github workflows , and controllers
    Signed-off-by: joseph Omer <jodeveloper8@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { HashtagsController } from './hashtags/hashtags.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [HashtagsController],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/users/users.controller.ts
@@ -0,0 +1,9 @@
+import { Controller } from '@nestjs/common';
+
+
+@Controller('users')
+export class UsersController {
+  constructor() { }
+
+
+}
