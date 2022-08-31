d096adaf86ddda320447d84d453aee5957263075
joseph Omer <jodeveloper8@gmail.com>
2022-08-31 15:23:36 +0300

    add all controllers and routes for hashtags controller

--- src/hashtags/hashtags.controller.ts
+++ src/hashtags/hashtags.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('hashtags')
-export class HashtagsController {}
+export class HashtagsController {
+  @Get('/')
+  getHashtags(): string {
+    // TODO: add actual logic
+    return 'all top hashtags';
+  }
+
+  @Get('/:tag/posts')
+  getPostsForHashtag(@Param() param): string {
+    // TODO: add actual logic
+    return `show all posts with hashtag ${param.tag}`;
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 
 @Controller('users')
 export class UsersController {
   constructor() { }
 
+  @Get('/@:username')
+  getUserByUsername(@Param() param): string {
+    // TODO
+    return `details of username = ${param.username}`;
+  }
 
+  @Get('/:userid')
+  getUserByUserid(@Param() param): string {
+    // TODO
+    return `details of user id = ${param.userid}`;
+  }
+
+  @Post('/')
+  createNewUser(): string {
+    return 'new user created';
+  }
+
+  @Patch('/:userid')
+  updateUserDetails(@Param() param): string {
+    return `details of user (id = ${param.userid}) updated`;
+  }
+
+  @Put('/:userid/follow')
+  followUser(): string {
+    return 'you followed user';
+  }
+
+  @Delete('/:userid/follow')
+  unfollowUser(): string {
+    return 'you unfollowed user';
+  }
+
+  @Get('/:userid/followers')
+  getFollowersOfUser(): string {
+    return 'get followers of user';
+  }
+
+  @Put('/:userid/followees')
+  getFolloweesOfUser(): string {
+    return `get followees of given user`;
+  }
 }
