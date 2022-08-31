06049684ab1c351b3315e9af12efbdbfcdddf554
joseph Omer <jodeveloper8@gmail.com>
2022-08-31 15:31:04 +0300

    post rest api done

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,8 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 
 @Controller('posts')
 export class PostsController {
   constructor() { }
+  @Get('/')
+  getAllPosts(): string {
+    // TODO
+    return 'get all posts';
+  }
 
+  @Get('/:postid')
+  getPostDetails(@Param('postid') postid: string): string {
+    // TODO
+    return `details of postid = ${postid}`;
+  }
+
+  @Post('/')
+  createNewPost(): string {
+    // TODO
+    return `new post was created`;
+  }
+
+  @Delete('/:postid')
+  deletePost(@Param('postid') postid: string): string {
+    // TODO
+    return `delete postid = ${postid}`;
+  }
+
+  @Put('/:postid/like')
+  likePost(@Param('postid') postid: string): string {
+    return `liked post ${postid}`;
+  }
+
+  @Delete('/:postid/like')
+  unlikePost(@Param('postid') postid: string): string {
+    return `unliked post ${postid}`;
+  }
 }
