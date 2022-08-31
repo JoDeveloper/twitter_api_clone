6a788da21d1ecce9cc5fdb31fbcfd9ab391a3009
joseph Omer <jodeveloper8@gmail.com>
2022-08-31 17:05:08 +0300

    refoctore files :tada:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { HashtagsController } from './hashtags/hashtags.controller';
+import { HashtagsController } from './hashtags/controllers/hashtags.controller';
 
 @Module({
   imports: [
